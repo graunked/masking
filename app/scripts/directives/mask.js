f1bae5efd92fdb571a0d9d41a63b4dcc5f677e4f
David Graunke <graunked@gmail.com>
2014-10-14 12:44:48 -0700

    Fix mask positioning

---
+++ app/scripts/directives/mask.js
@@ -0,0 +1,31 @@
+'use strict';
+
+angular.module('scrollviewTestApp')
+  .directive('mask', function ($famous, $famousDecorator) {
+    return {
+    transclude: true,
+    templateUrl: 'views/mask.html',
+    restrict: 'E',
+    scope: {faSize: '&', faTranslate: '&', faRotateZ: '&', contentSize: '&'},
+    controller: function($scope) {
+
+      $scope.offset = function() {
+        var size = $scope.faSize();
+        var translate = $scope.faTranslate();
+        var t = [(size[0] / 2) - translate[0] * 8, (size[1] / 2) - translate[1]];
+        console.log(t);
+        return $scope.vectorNegate($scope.faTranslate());
+      };
+
+      $scope.origin = function() {
+        var offset = $scope.offset();
+        var content = $scope.contentSize();
+        return [offset[0] / content[0], offset[1] / content[1]];
+      };
+
+      $scope.vectorNegate = function(v) {
+        return [-v[0], -v[1]];
+      };
+    }
+  };
+});
