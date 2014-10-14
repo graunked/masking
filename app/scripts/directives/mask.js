30e6cd2b3a8e2aa9df110e8c106fdad55c00740a
David Graunke <graunked@gmail.com>
2014-10-14 16:50:45 -0700

    Fix translation

--- app/scripts/directives/mask.js
+++ app/scripts/directives/mask.js
@@ -8,21 +8,17 @@ angular.module('scrollviewTestApp')
     restrict: 'E',
     scope: {faSize: '&', faTranslate: '&', faRotateZ: '&', contentSize: '&'},
     controller: function($scope) {
-
       $scope.offset = function() {
         var size = $scope.faSize();
         var translate = $scope.faTranslate();
-        var t = [(size[0] / 2) - translate[0] * 8, (size[1] / 2) - translate[1]];
-        console.log(t);
-        return $scope.vectorNegate($scope.faTranslate());
+        var t = [size[0] / 2 - 1000, size[1] / 2 - translate[1]* 2];
+        return t;
       };
-
       $scope.origin = function() {
         var offset = $scope.offset();
         var content = $scope.contentSize();
         return [offset[0] / content[0], offset[1] / content[1]];
       };
-
       $scope.vectorNegate = function(v) {
         return [-v[0], -v[1]];
       };
